b466060dd3bbd96c7bb8dd1277e1108efca2ba35
unknown <Dmitry@.(none)>
2011-08-28 01:01:27 -0400

    Changing report formatting.

--- C++/E_Reporter.cpp
+++ C++/E_Reporter.cpp
@@ -9,9 +9,11 @@ using std::endl;
 // reports a method call
 void report_method(const string& method_name, const string& class_name, const ReportStage stage) {
     if (stage == PROLOGUE) {
-        cerr << "/* P: \"" << method_name << "\" on one of \"" << class_name << "\"" << endl;
+        cerr << "/*** P-METH: \"" << method_name << "\" on object \"" << class_name << "\"" << endl;
+        cerr << "|" << endl;
     } else if (stage == EPILOGUE) {
-        cerr << "\\* E: \"" << method_name << "\" on one of \"" << class_name << "\"" << endl;
+        cerr << "|" << endl;
+        cerr << "\\*** E-METH: \"" << method_name << "\" on object \"" << class_name << "\"" << endl;
     }
     return;
 }
@@ -19,9 +21,11 @@ void report_method(const string& method_name, const string& class_name, const Re
 // reports a constructor call
 void report_constructor(const string& class_name, const ReportStage stage) {
     if (stage == PROLOGUE) {
-        cerr << "/* P-CSTR: \"" << class_name << "\"" << endl;
+        cerr << "/*** P-CSTR: \"" << class_name << "\"" << endl;
+        cerr << "|" << endl;
     } else if (stage == EPILOGUE) {
-        cerr << "\\* E-CSTR: \"" << class_name << "\"" << endl;
+        cerr << "|" << endl;
+        cerr << "\\*** E-CSTR: \"" << class_name << "\"" << endl;
     }
     return;
 }
@@ -29,9 +33,11 @@ void report_constructor(const string& class_name, const ReportStage stage) {
 // reports a destructor call
 void report_destructor(const string& class_name, const ReportStage stage) {
     if (stage == PROLOGUE) {
-        cerr << "/* P-DSTR: \"" << class_name << "\"" << endl;
+        cerr << "/*** P-DSTR: \"" << class_name << "\"" << endl;
+        cerr << "|" << endl;
     } else if (stage == EPILOGUE) {
-        cerr << "\\* E-DSTR: \"" << class_name << "\"" << endl;
+        cerr << "|" << endl;
+        cerr << "\\*** E-DSTR: \"" << class_name << "\"" << endl;
     }
     return;
 }
