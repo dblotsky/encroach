46029e95ecc78f34e082e1edba3128e0330844e4
Dmitry Blotsky (VPS) <dmitry.blotsky@gmail.com>
2011-08-28 02:56:15 +0000

    Added new files. Added gitignore.

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "E_Model.h"
+#include "E_Controller.h"
+#include "E_View.h"
+#include "E_Terminal.h"
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    printf("Hello, world!\n");
+
+    // make a back-end
+    E_Model*        model       = new E_Model();
+    E_Controller*   controller  = new E_Controller(model);
+    
+    // make a front-end
+    E_Terminal* view = new E_Terminal(controller, model);
+    
+    // run the game
+    view->run();
+    
+    // delete the front-end
+    delete view;
+    
+    // delete the back-end
+    delete controller;
+    delete model;
+
     return 0;
 } // main
