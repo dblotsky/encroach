0bcaee32e32e2c90b7750393ba281332ee77eafa
unknown <Dmitry@.(none)>
2011-08-28 00:42:10 -0400

    Fixing compile errors.

--- C++/E_Reporter.cpp
+++ C++/E_Reporter.cpp
@@ -3,6 +3,8 @@
 #include "E_Reporter.h"
 
 using std::string;
+using std::cerr;
+using std::endl;
 
 // reports a method call
 void report_method(const string& method_name, const string& class_name, const ReportStage stage) {
@@ -15,7 +17,7 @@ void report_method(const string& method_name, const string& class_name, const Re
 }
 
 // reports a constructor call
-void report_constructor(const string& class_name) {
+void report_constructor(const string& class_name, const ReportStage stage) {
     if (stage == PROLOGUE) {
         cerr << "Started creating an object of class \"" << class_name << ".\"" << endl;
     } else if (stage == EPILOGUE) {
@@ -25,7 +27,7 @@ void report_constructor(const string& class_name) {
 }
 
 // reports a destructor call
-void report_destructor(const string& class_name) {
+void report_destructor(const string& class_name, const ReportStage stage) {
     if (stage == PROLOGUE) {
         cerr << "Started deleting an object of class \"" << class_name << ".\"" << endl;
     } else if (stage == EPILOGUE) {
