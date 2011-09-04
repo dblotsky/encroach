acd7c7b436c8f33a868998b9f94640bf8a070b4b
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2011-09-04 13:34:38 -0400

    Removed the register_player method from E_Controller.

--- C++/E_Controller.cpp
+++ C++/E_Controller.cpp
@@ -1,5 +1,4 @@
 #include "E_Reporter.h"
-
 #include "E_Controller.h"
 #include "E_Model.h"
 
@@ -24,11 +23,6 @@ void E_Controller::new_ai_game(const string& player_name) {
 void E_Controller::new_human_game(const string& player1_name, const string& player2_name) {
     prologue("E_Controller", "new_human_game");
     epilogue("E_Controller", "new_human_game");
-}
-
-void E_Controller::register_player(const string& player_name) {
-    prologue("E_Controller", "register_player");
-    epilogue("E_Controller", "register_player");
 }
 
 void E_Controller::event_move(const string& command_string) {
--- C++/E_Controller.h
+++ C++/E_Controller.h
@@ -2,7 +2,6 @@
 #define _E_CONTROLLER_
 
 #include <string>
-
 #include "E_Model.h"
 
 using std::string;
@@ -15,7 +14,6 @@ class E_Controller {
         // events
         void new_ai_game(const string& player_name);
         void new_human_game(const string& player1_name, const string& player2_name);
-        void register_player(const string& player_name);
         void event_move(const string& command_string);
         
     private:
