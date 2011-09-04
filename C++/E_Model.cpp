45e0ca32fbd420a2ec79807a0e2c67e3c2aaa9e6
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2011-09-04 13:33:39 -0400

    Added game and players to model.

--- C++/E_Model.cpp
+++ C++/E_Model.cpp
@@ -1,9 +1,16 @@
+#include <vector>
 #include "E_Reporter.h"
-
 #include "E_Model.h"
+#include "E_Board.h"
+#include "E_Player.h"
+
+using std::vector;
 
 E_Model::E_Model() {
     prologue("E_Model");
+    
+    board = NULL;
+    
     epilogue("E_Model");
 }
 
--- C++/E_Model.h
+++ C++/E_Model.h
@@ -1,7 +1,12 @@
 #ifndef _E_MODEL_
 #define _E_MODEL_
 
+#include <vector>
 #include "E_Subject.h"
+#include "E_Board.h"
+#include "E_Player.h"
+
+using std::vector;
 
 class E_Model : public E_Subject {
     public:
@@ -9,7 +14,8 @@ class E_Model : public E_Subject {
         ~E_Model();
         
     private:
-        
+        E_Board* board;
+        vector<E_Player*> players;
 };
 
 #endif
