3c2fc3a13388e35ac342d79337982385eeaa05c8
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2011-09-19 00:08:29 -0400

    Added a command line option parser.

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,9 +1,24 @@
+#include <string>
+#include "E_Reporter.h"
 #include "E_Model.h"
 #include "E_Controller.h"
 #include "E_View.h"
 #include "E_Terminal.h"
 
+void process_options(int argc, char* argv[]) {
+    std::string option;
+    for (int i = 0; i < argc; i++){
+        option = argv[i];
+        if (option == "-d" ||
+            option == "--debug") {
+            on_debug();
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
+    
+    process_options(argc, argv);
 
     // create a back-end
     E_Model*        model       = new E_Model();
